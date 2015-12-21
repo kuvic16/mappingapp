762aef9b9fd50d37ee4f7567fcf2ddd9435029a7
Shaiful Islam <kuvic16@gmail.com>
2015-12-21 10:44:53 +0600

    added few links

--- protected/views/userFile/map.php
+++ protected/views/userFile/map.php
@@ -1,8 +1,8 @@
 <script src="http://maps.google.com/maps/api/js?sensor=true&libraries=places" type="text/javascript"></script>
-  <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" />
-  <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" />
+<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
 <?php
 /* @var $this UserFileController */
 /* @var $model UserFile */
@@ -24,95 +24,96 @@ $this->breadcrumbs = array(
     $("#searchBox").hide();
     var locations = [
 <?php
-$row = 1; $Index = 0;
+$row = 1;
+$Index = 0;
 $length = count($model->csv_data);
 if ($length > 1) {
     foreach ($model->csv_data as $data) {
         if ($row !== 1) {
             ?>
-            [
-                <?php 
-                    if(strlen(trim($model->name_index))> 0){
-                        echo "'" . $data[$model->name_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->address_index))> 0){
-                        echo "'" . $data[$model->address_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->city_index))> 0){
-                        echo "'" . $data[$model->city_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->state_index))> 0){
-                        echo "'" . $data[$model->state_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->zipcode_index))> 0){
-                        echo "'" . $data[$model->zipcode_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->phone_index))> 0){
-                        echo "'" . $data[$model->phone_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->field1_index))> 0){
-                        echo "'" . $data[$model->field1_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->field2_index))> 0){
-                        echo "'" . $data[$model->field2_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->field3_index))> 0){
-                        echo "'" . $data[$model->field3_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                <?php 
-                    if(strlen(trim($model->field4_index))> 0){
-                        echo "'" . $data[$model->field4_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,            
-                <?php 
-                    if(strlen(trim($model->field5_index))> 0){
-                        echo "'" . $data[$model->field5_index] . "'"; 
-                    }  else {
-                        echo "'NULL'"; 
-                    }
-                ?>,
-                "'<?php echo $Index; ?>'",
-            ],
+                    [
+            <?php
+            if (strlen(trim($model->name_index)) > 0) {
+                echo "'" . $data[$model->name_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->address_index)) > 0) {
+                echo "'" . $data[$model->address_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->city_index)) > 0) {
+                echo "'" . $data[$model->city_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->state_index)) > 0) {
+                echo "'" . $data[$model->state_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->zipcode_index)) > 0) {
+                echo "'" . $data[$model->zipcode_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->phone_index)) > 0) {
+                echo "'" . $data[$model->phone_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->field1_index)) > 0) {
+                echo "'" . $data[$model->field1_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->field2_index)) > 0) {
+                echo "'" . $data[$model->field2_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->field3_index)) > 0) {
+                echo "'" . $data[$model->field3_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->field4_index)) > 0) {
+                echo "'" . $data[$model->field4_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+            <?php
+            if (strlen(trim($model->field5_index)) > 0) {
+                echo "'" . $data[$model->field5_index] . "'";
+            } else {
+                echo "'NULL'";
+            }
+            ?>,
+                            "'<?php echo $Index; ?>'",
+                    ],
             <?php
             $Index++;
-           }
+        }
         $row++;
     }
 }
@@ -120,11 +121,11 @@ if ($length > 1) {
     ];
 
     var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 7,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-    var infowindowlist = new Array();        
-    var markerlist = new Array();        
+    var infowindowlist = new Array();
+    var markerlist = new Array();
     function initMap() {
         google.maps.Map.prototype.clearInfoWindow = function () {
             for (var i = 0; i < infowindowlist.length; i++) {
@@ -133,24 +134,20 @@ if ($length > 1) {
             }
         };
 
-//        var map = new google.maps.Map(document.getElementById('map'), {
-//            zoom: 7,
-//            mapTypeId: google.maps.MapTypeId.ROADMAP
-//        });
         var geocoder = new google.maps.Geocoder();
         var i;
         for (i = 0; i < locations.length; i++) {
             var address = locations[i][1].split(">");
-            if(address.length <= 1){
+            if (address.length <= 1) {
                 geocode(geocoder, locations[i], i, function (results, i) {
                     addMarker(map, results[0].geometry.location, locations[i], i);
                 });
-            }else{
-                var latLng = new google.maps.LatLng(parseFloat(address[1]),parseFloat(address[2]));
+            } else {
+                var latLng = new google.maps.LatLng(parseFloat(address[1]), parseFloat(address[2]));
                 addMarker(map, latLng, locations[i], i);
             }
         }
-        
+
         //search box
         // Create the search box and link it to the UI element.
         var input = document.getElementById('searchBox');
@@ -158,8 +155,8 @@ if ($length > 1) {
         map.controls[google.maps.ControlPosition.RIGHT].push(input);
 
         // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          //searchBox.setBounds(map.getBounds());
+        map.addListener('bounds_changed', function () {
+            //searchBox.setBounds(map.getBounds());
         });
 
     }
@@ -180,29 +177,29 @@ if ($length > 1) {
                 '<br/><b>City:</b> <input class="<?php echo $model->city_index; ?> 2 map-text-box" value="' + locations[2] + '"></input>' +
                 '<br/><b>State:</b> <input class="<?php echo $model->state_index; ?> 3 map-text-box margin-right-3" value="' + locations[3] + '"></input>' +
                 '<br/><b>Zipcode:</b> <input class="<?php echo $model->zipcode_index; ?> 4 map-text-box" value="' + locations[4] + '"></input>';
-        if(locations[5] !== 'NULL'){
+        if (locations[5] !== 'NULL') {
             contentString = contentString + '<br/><b>Phone:</b><input class="<?php echo $model->phone_index; ?> 5 map-text-box" value="' + locations[5] + '"></input>';
         }
-        if(locations[6] !== 'NULL'){
+        if (locations[6] !== 'NULL') {
             contentString = contentString + '<br/><b><?php echo $model->field1_label; ?>:</b><input class="<?php echo $model->field1_index; ?> 6 map-text-box" value="' + locations[6] + '"></input>';
         }
-        if(locations[7] !== 'NULL'){
+        if (locations[7] !== 'NULL') {
             contentString = contentString + '<br/><b><?php echo $model->field2_label; ?>:</b><input class="<?php echo $model->field2_index; ?> 7 map-text-box" value="' + locations[7] + '"></input>';
         }
-        if(locations[8] !== 'NULL'){
+        if (locations[8] !== 'NULL') {
             contentString = contentString + '<br/><b><?php echo $model->field3_label; ?>:</b><input class="<?php echo $model->field3_index; ?> 8 map-text-box" value="' + locations[8] + '"></input>';
         }
-        if(locations[9] !== 'NULL'){
+        if (locations[9] !== 'NULL') {
             contentString = contentString + '<br/><b><?php echo $model->field4_label; ?>:</b><input class="<?php echo $model->field4_index; ?> 9 map-text-box" value="' + locations[9] + '"></input>';
         }
-        if(locations[10] !== 'NULL'){
+        if (locations[10] !== 'NULL') {
             contentString = contentString + '<br/><b><?php echo $model->field5_label; ?>:</b><input class="<?php echo $model->field5_index; ?> 10 map-text-box" value="' + locations[10] + '"></input>';
         }
         contentString = contentString + '</div>';
         contentString = contentString + '<div class="button-section">';
         contentString = contentString + '<span id="info" style="float: left"></span>';
         contentString = contentString + '<input type="button" class="new_btn bottom_btn" value="Cancel" onclick="cancelEditRequest()"></input>';
-        contentString = contentString + '<input class="new_btn bottom_btn" type="button" value="Save" onclick="saveRequest('+ i +')"></input>';
+        contentString = contentString + '<input class="new_btn bottom_btn" type="button" value="Save" onclick="saveRequest(' + i + ')"></input>';
         contentString = contentString + '</div>' + '</div>';
 
         var infowindow = new google.maps.InfoWindow({
@@ -224,7 +221,7 @@ if ($length > 1) {
         geocoder.geocode({'address': location[1] + "," + location[2] + "," + location[3] + " " + location[4]}, function (results, status) {
             if (status === google.maps.GeocoderStatus.OK) {
                 if (typeof callback === "function") {
-                    callServer(results[0].geometry.location.lat(), results[0].geometry.location.lng(), i+2);
+                    callServer(results[0].geometry.location.lat(), results[0].geometry.location.lng(), i + 2);
                     callback(results, i);
                 }
             } else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
@@ -232,7 +229,7 @@ if ($length > 1) {
                     geocode(geocoder, location, i, callback);
                 }, 20);
             } else {
-                console.error('Geocode for: '+ i + location[1]+' was not successful for the following reason: ' + status);
+                console.error('Geocode for: ' + i + location[1] + ' was not successful for the following reason: ' + status);
             }
         });
     }
@@ -257,101 +254,70 @@ if ($length > 1) {
             }
         });
     }
-    
-    function changeRequest(row_id, server_column_id, local_column_id, element){
-        var prev_value = locations[row_id][local_column_id];
-        var new_value = $(element).val();
-        
-        if(local_column_id === '1'){
-            prev_value = locations[row_id][local_column_id].split(">")[0];
-        }
-        if(prev_value.trim() !== new_value.trim()){
-            //$("#ntf").show();
-//            $.ajax({
-//                type: "POST",
-//                url: "<?php echo Yii::app()->createUrl('userFile/dataUpdate'); ?>",
-//                data: {
-//                    file_name: "<?php echo $model->physical_file_name; ?>",
-//                    row_id: row_id,
-//                    column_id: server_column_id,
-//                    column_value: $(element).val(),
-//                    id: "<?php echo $model->id; ?>",
-//                },
-//                success: function (msg) {
-//                    locations[row_id][local_column_id] = $(element).val();
-//                    $("#ntf").hide();
-//                },
-//                error: function (xhr) {
-//                    console.log("failure" + xhr.readyState + this.url);
-//                    $("#ntf").hide();
-//                }
-//            });
-        }
-    }
 </script>
 
-  <script>
+<script>
     var element = $("#searchBox");
     element.autocomplete({
-      source: function(request, response) {
+        source: function (request, response) {
             var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
-            response($.grep(locations, function(value) {
+            response($.grep(locations, function (value) {
                 return matcher.test(value[0]);
             }));
         },
-        select: function(event, ui) {
+        select: function (event, ui) {
             element.val(ui.item[0]);
-            infowindow = new google.maps.InfoWindow(); 
+            infowindow = new google.maps.InfoWindow();
             infowindow = infowindowlist[ui.item[11]];
             marker = new google.maps.Marker();
-            marker = markerlist[ui.item[11]]; 
-            var latLng = new google.maps.LatLng(parseFloat(ui.item[1].split(">")[1]),parseFloat(ui.item[1].split(">")[2]));
+            marker = markerlist[ui.item[11]];
+            var latLng = new google.maps.LatLng(parseFloat(ui.item[1].split(">")[1]), parseFloat(ui.item[1].split(">")[2]));
             map.setCenter(latLng);
             map.setZoom(15);
 //            infowindow.open(map, marker);
 //            google.maps.event.trigger(marker, 'click');
             return false;
         }
-    }).autocomplete( "instance" )._renderItem = function( ul, item ) {
-      return $( "<li>" )
-        .append( "<p class='searchBoxHeader'>" + item[0] + "</p><p class='searchBoxDetails'>" + item[1].split(">")[0] + ", " + item[2]+ ", " + item[3]+ ", " + item[4] + "</p>" )
-        .appendTo( ul );
+    }).autocomplete("instance")._renderItem = function (ul, item) {
+        return $("<li>")
+                .append("<p class='searchBoxHeader'>" + item[0] + "</p><p class='searchBoxDetails'>" + item[1].split(">")[0] + ", " + item[2] + ", " + item[3] + ", " + item[4] + "</p>")
+                .appendTo(ul);
     };
     //$("#searchBox").show();
-    function editRequest(){
+    function editRequest() {
         clearInfoMessage();
         $(".map-text-box").removeAttr('disabled', 'disabled');
-        $(".map-text-box").addClass("map-text-box-edit");        
+        $(".map-text-box").addClass("map-text-box-edit");
         $(".bottom_btn").show();
     }
-    
-    function cancelEditRequest(){
+
+    function cancelEditRequest() {
         $(".map-text-box").attr('disabled', 'disabled');
-        $(".map-text-box").removeClass("map-text-box-edit");        
+        $(".map-text-box").removeClass("map-text-box-edit");
         $(".bottom_btn").hide();
     }
-    
-    function setInfoMessage(msg){
+
+    function setInfoMessage(msg) {
         $("#info").html(msg);
     }
-    
-    function clearInfoMessage(){
+
+    function clearInfoMessage() {
         $("#info").html("");
     }
-    
-    function saveRequest(row_id){
+
+    function saveRequest(row_id) {
         jsonObj = [];
-        $('.map-text-box').each(function(){
+        $('.map-text-box').each(function () {
             classes = $(this).attr('class').split(' ');
             item = {}
             item ["row_id"] = row_id;
             item ["column_id"] = classes[0];
             item ["column_value"] = $(this).val();
             item ["local_column_id"] = classes[1];
-            jsonObj.push(item);            
+            jsonObj.push(item);
         });
         //console.log(jsonObj);
-        
+
         $("#ntf").show();
         $.ajax({
             type: "POST",
@@ -371,7 +337,7 @@ if ($length > 1) {
                 console.log("failure" + xhr.readyState + this.url);
                 $("#ntf").hide();
             }
-        });        
+        });
     }
     clearInfoMessage();
-   </script>
+</script>
