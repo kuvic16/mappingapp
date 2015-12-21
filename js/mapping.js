2872c5017e6713a35897d5bcc3a9b1ecd00153cf
Shaiful Islam <kuvic16@gmail.com>
2015-12-21 13:52:04 +0600

    added few links

---
+++ js/mapping.js
@@ -0,0 +1,195 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var infowindowlist = new Array();
+var markerlist = new Array();
+var gm = google.maps;
+var searchBoxElement = $("#searchBox");
+var map;
+
+window.onload = function () {
+    map = new gm.Map(document.getElementById('map'), {
+        mapTypeId: gm.MapTypeId.ROADMAP,
+        center: new gm.LatLng(50, 0),
+        zoom: 6,
+    });
+    var iw = new gm.InfoWindow();
+    var oms = new OverlappingMarkerSpiderfier(map, {markersWontMove: true, markersWontHide: true});
+
+    var usualColor = 'eebb22';
+    //var usualColor = 'FE7569';
+    var spiderfiedColor = 'ffee22';
+    //var spiderfiedColor = 'F78181';
+    oms.addListener('click', function (marker) {
+        iw.setContent(marker.desc);
+        iw.open(map, marker);
+        $("#ntf").hide();
+        cancelEditRequest();
+        
+    });
+    oms.addListener('spiderfy', function (markers) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setIcon(iconWithColor(spiderfiedColor));
+            markers[i].setShadow(null);
+        }
+        iw.close();
+    });
+    oms.addListener('unspiderfy', function (markers) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setIcon(iconWithColor(usualColor));
+            markers[i].setShadow(shadow);
+        }
+    });
+
+    var bounds = new gm.LatLngBounds();
+    var geocoder = new google.maps.Geocoder();
+    for (i = 0; i < locations.length; i++) {
+        var address = locations[i][1].split(">");
+        if (address.length <= 1) {
+            geocode(geocoder, locations[i], i, function (results, i) {
+                bounds.extend(results[0].geometry.location);
+                addMarker(map, results[0].geometry.location, locations[i], i, oms, usualColor, shadow);
+            });
+        } else {
+            var loc = new gm.LatLng(parseFloat(address[1]), parseFloat(address[2]));
+            bounds.extend(loc);
+            addMarker(map, loc, locations[i], i, oms, usualColor, shadow);
+        }
+    }
+    map.fitBounds(bounds);
+
+    var input = document.getElementById('searchBox');
+    map.controls[google.maps.ControlPosition.RIGHT].push(input);
+    map.addListener('bounds_changed', function () {
+        //searchBox.setBounds(map.getBounds());
+    });
+
+    window.map = map;
+    window.oms = oms;
+}
+
+
+var iconWithColor = function (color) {
+    //return 'http://chart.googleapis.com/chart?chst=d_map_xpin_letter&chld=pin|+|' + color + '|000000|ffff00';
+    return 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + color + '|000000|ffff00';
+}
+
+var shadow = new gm.MarkerImage(
+        'https://www.google.com/intl/en_ALL/mapfiles/shadow50.png',
+        new gm.Size(37, 34), // size   - for sprite clipping
+        new gm.Point(0, 0), // origin - ditto
+        new gm.Point(10, 34)  // anchor - where to meet map location
+        );
+
+google.maps.Map.prototype.clearInfoWindow = function () {
+    for (var i = 0; i < infowindowlist.length; i++) {
+        if (infowindowlist[i])
+            infowindowlist[i].close();
+    }
+};
+
+function addMarker(map, latlon, locations, i, oms, usualColor, shadow) {
+    var marker = new gm.Marker({
+        map: map,
+        position: latlon,
+        title: locations[0],
+        icon: iconWithColor(usualColor),
+        shadow: shadow
+    });
+    var contentString = '<div id="content">' +
+            '<div style="width: 16px; float: right"><img src="' + baseUrl + '/css/ajax-loader.gif" id="ntf" class="ntf"></img></div>' +
+            '<img onclick="editRequest()" src="' + baseUrl + '/css/edit.png" class="edit"></img>' +
+            '<input class="' + nameIndex + ' 0 map-text-box firstHeading" id="firstHeading" value="' + locations[0] + '"></input>' +
+            '<div id="bodyContent" class="bodyContent">' +
+            '<b>Address:</b> <input class="' + addressIndex + ' 1 map-text-box" value="' + locations[1].split(">")[0] + '"></input>' +
+            '<br/><b>City:</b> <input class="' + cityIndex + ' 2 map-text-box" value="' + locations[2] + '"></input>' +
+            '<br/><b>State:</b> <input class="' + stateIndex + ' 3 map-text-box margin-right-3" value="' + locations[3] + '"></input>' +
+            '<br/><b>Zipcode:</b> <input class="' + zipcodeIndex + ' 4 map-text-box" value="' + locations[4] + '"></input>';
+    if (locations[5] !== 'NULL') {
+        contentString = contentString + '<br/><b>Phone:</b><input class="' + phoneIndex + ' 5 map-text-box" value="' + locations[5] + '"></input>';
+    }
+    if (locations[6] !== 'NULL') {
+        contentString = contentString + '<br/><b>' + field1Label + ':</b><input class="' + field1Index + ' 6 map-text-box" value="' + locations[6] + '"></input>';
+    }
+    if (locations[7] !== 'NULL') {
+        contentString = contentString + '<br/><b>' + field2Label + ':</b><input class="' + field2Index + ' 7 map-text-box" value="' + locations[7] + '"></input>';
+    }
+    if (locations[8] !== 'NULL') {
+        contentString = contentString + '<br/><b>' + field3Label + ':</b><input class="' + field3Index + ' 8 map-text-box" value="' + locations[8] + '"></input>';
+    }
+    if (locations[9] !== 'NULL') {
+        contentString = contentString + '<br/><b>' + field4Label + ':</b><input class="' + field4Index + ' 9 map-text-box" value="' + locations[9] + '"></input>';
+    }
+    if (locations[10] !== 'NULL') {
+        contentString = contentString + '<br/><b>' + field5Label + ':</b><input class="' + field5Index + ' 10 map-text-box" value="' + locations[10] + '"></input>';
+    }
+    contentString = contentString + '</div>';
+    contentString = contentString + '<div class="button-section">';
+    contentString = contentString + '<span id="info" style="float: left"></span>';
+    contentString = contentString + '<input type="button" class="new_btn bottom_btn" value="Cancel" onclick="cancelEditRequest()"></input>';
+    contentString = contentString + '<input class="new_btn bottom_btn" type="button" value="Save" onclick="saveRequest(' + i + ')"></input>';
+    contentString = contentString + '</div>' + '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    infowindowlist[infowindowlist.length] = infowindow;
+    markerlist[markerlist.length] = marker;
+
+    marker.desc = contentString;
+    oms.addMarker(marker);
+//        marker.addListener('click', function () {
+//            map.clearInfoWindow();
+//            infowindow.open(map, marker);
+//            $("#ntf").hide();
+//            cancelEditRequest();
+//        });
+}
+
+// call geocode for lat and lon
+function geocode(geocoder, location, i, callback) {
+    geocoder.geocode({'address': location[1] + "," + location[2] + "," + location[3] + " " + location[4]}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (typeof callback === "function") {
+                callServer(results[0].geometry.location.lat(), results[0].geometry.location.lng(), i + 2);
+                callback(results, i);
+            }
+        } else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+            setTimeout(function () {
+                geocode(geocoder, location, i, callback);
+            }, 20);
+        } else {
+            console.error('Geocode for: ' + i + location[1] + ' was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+searchBoxElement.autocomplete({
+    source: function (request, response) {
+        var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+        response($.grep(locations, function (value) {
+            return matcher.test(value[0]);
+        }));
+    },
+    select: function (event, ui) {
+        searchBoxElement.val(ui.item[0]);
+//        infowindow = new google.maps.InfoWindow();
+//        infowindow = infowindowlist[ui.item[11]];
+//        marker = new google.maps.Marker();
+//        marker = markerlist[ui.item[11]];
+        var latLng = new google.maps.LatLng(parseFloat(ui.item[1].split(">")[1]), parseFloat(ui.item[1].split(">")[2]));
+        map.setCenter(latLng);
+        map.setZoom(15);
+//            infowindow.open(map, marker);
+//            google.maps.event.trigger(marker, 'click');
+        return false;
+    }
+}).autocomplete("instance")._renderItem = function (ul, item) {
+    return $("<li>")
+            .append("<p class='searchBoxHeader'>" + item[0] + "</p><p class='searchBoxDetails'>" + item[1].split(">")[0] + ", " + item[2] + ", " + item[3] + ", " + item[4] + "</p>")
+            .appendTo(ul);
+};
